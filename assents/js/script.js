40d4a8219159e7ff9736e19e2a4c055596569b74
Ianbarbosa1 <iancosta.tec@gmail.com>
2025-12-01 01:54:48 -0300

    Alteração do link do cliente

--- assents/js/script.js
+++ assents/js/script.js
@@ -126,7 +126,7 @@ async function SalvarDados(e) {
             + 'Parabéns! Você está participando do sorteio do Bazar Inovar' + '%0a'
             + 'O sorteio será realizado no dia 30/12/2025.' + '%0a'
             + 'Consulte o link para saber mais informações sobre quais serão os premios!' + '%0a'
-            + 'link do sorteio' + '%0a'
+            + 'https://sl1nk.com/Fim-de-Ano-Premiado' + '%0a'
         window.open(url, '_blank').focus()
     }
 
